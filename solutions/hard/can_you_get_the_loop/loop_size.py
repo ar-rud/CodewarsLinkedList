02ab50909d1a144635658ec53988b9ff2fb31874
artur_rudish <ar194225@gmail.com>
2024-04-08 20:38:49 +0300

    implemented loop size function, added node class, attached photo proof of solution

---
+++ solutions/hard/can_you_get_the_loop/loop_size.py
@@ -0,0 +1,34 @@
+"""
+loop size
+"""
+
+class Node:
+    """node class"""
+
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+def loop_size(node):
+    fast = node
+    slow = node
+    counter = 0
+
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+        if fast == slow:
+            slow = node
+
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next
+
+            leng = 1
+            fast = fast.next
+            while fast != slow:
+                fast = fast.next
+                leng += 1
+
+            return leng
